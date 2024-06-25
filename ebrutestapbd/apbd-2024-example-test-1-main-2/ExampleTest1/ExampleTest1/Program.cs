ecd997ce4e4bcb2f065ec10a82e68da1532282dd
kurapikasphatbum <yilmazebru2002@gmail.com>
2024-06-25 11:26:26 +0200

    finall

--- ebrutestapbd/apbd-2024-example-test-1-main-2/ExampleTest1/ExampleTest1/Program.cs
+++ ebrutestapbd/apbd-2024-example-test-1-main-2/ExampleTest1/ExampleTest1/Program.cs
@@ -3,7 +3,6 @@ using ExampleTest2.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddScoped<IMoviesRepository, MoviesRepository>();
 builder.Services.AddScoped<IMovieService, MovieService>();
 
@@ -13,7 +12,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
